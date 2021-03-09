c8244de609a5b2194052dd5d04a3829f17240c8d
Charles De Mount <thexrayone@icloud.com>
2021-03-09 13:35:35 -0500

    add css module declaration

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am the coolest person in my room. Most definitely.</p>
+        <p>I am the coolest person in my room. Probably.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
